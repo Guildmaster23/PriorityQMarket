6ebccf7f2dc00d202d4019a2be35506975c0525b
alex <xanderfomenko@gmail.com>
2016-09-21 21:59:11 +0300

    Big shit to do 2

--- src/Assets.java
+++ src/Assets.java
@@ -9,4 +9,20 @@ public class Assets {
         this.count = count;
         this.price = price;
     }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/Investor.java
+++ src/Investor.java
@@ -1,15 +1,32 @@
+import java.util.Random;
+
 /**
  * Created by 01 on 21.09.2016.
  */
 public class Investor {
+
+    private static int invesorsCount = 0;
+
+    public Assets portfolio;
+
     private int id;
-    static int invesorsCount = 0;
+    private Random r = new Random();
 
     public Investor() {
         this.id = invesorsCount++;
+
+        // debug
+        System.out.println("Generated Investor " + this.id + "with assets");
     }
 
     public int getId() {
         return id;
+    }
+
+    public void inheritFortune() {
+        portfolio = new Assets(r.nextInt(100), r.nextInt(100));
+
+        // debug
+        System.out.println("Stock portfolio now is: " + portfolio.getCount() + " at " + portfolio.getPrice());
     }
 }
